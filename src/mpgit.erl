c447ad7a888cf88328018f9a5158ad5410d5e119
Tuan Tran <mptnt1988@gmail.com>
2017-10-15 00:16:54 -0700

    Add first setup

--- src/mpgit.erl
+++ src/mpgit.erl
@@ -1,4 +1,5 @@
 -module(mpgit).
+-compile(export_all).
 
 %% API exports
 -export([]).
@@ -6,7 +7,15 @@
 %%====================================================================
 %% API functions
 %%====================================================================
-
+test() ->
+    lager:debug("Tuan debug"),
+    lager:info("Tuan info"),
+    lager:notice("Tuan notice"),
+    lager:warning("Tuan warning"),
+    lager:error("Tuan error"),
+    lager:critical("Tuan critical"),
+    lager:alert("Tuan alert"),
+    lager:emergency("Tuan emergency").
 
 %%====================================================================
 %% Internal functions
